8a79e426bbf789c1ec0f303c5b2f7745b877d28f
Jsmond2016 <Jsmond2016@gmail.com>
2022-05-28 17:35:22 +0800

    feat: mock 深入-使用 jest.mock 模块，结合 __mocks__ 通过测试

---
+++ lesson-mock/__mocks__/mock.js
@@ -0,0 +1,7 @@
+
+const fetchData = () => new Promise((resolve, reject) => resolve('123'))
+
+
+module.exports = {
+  fetchData
+}
--- lesson-mock/mock.test.js
+++ lesson-mock/mock.test.js
@@ -1,12 +1,24 @@
-const { fetchData } = require('./mock')
-const axios = require('axios')
 
-jest.mock('axios')
+// 模拟 mock 模块文件，要求 __mocks__ 目录下需要同名文件和内容
+jest.mock('./mock')
+// 同理，使用 jest.unmock('./mock') 取消模拟
+const { fetchData } = require('./mock')
+
+
+// const axios = require('axios')
+
+// jest.mock('axios')
+
+// test('mock-文件内-fetchData 测试', () => {
+//   axios.get.mockResolvedValue({
+//     data: '123'
+//   })
+//   return fetchData().then(data => {
+//     expect(data).toEqual('123')
+//   })
+// })
 
 test('mock-文件内-fetchData 测试', () => {
-  axios.get.mockResolvedValue({
-    data: '123'
-  })
   return fetchData().then(data => {
     expect(data).toEqual('123')
   })
